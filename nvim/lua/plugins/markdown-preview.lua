02f4207ce9932f9672a5c62dcf1f4b69f923afe0
jean-christophe Maigrot <jean-christophe.maigrot@steeple.fr>
2024-06-08 18:52:21 +0200

    feat(nvim): install markdown-preview.nvim

---
+++ nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,10 @@
+return {
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && yarn install",
+	ft = { "markdown" },
+	config = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+		vim.keymap.set("n", "<leader>mp", vim.cmd.MarkdownPreview, {})
+	end,
+}
