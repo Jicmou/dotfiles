6e43a5fdf611662ea61aefc8c4fd4229c0070660
jean-christophe Maigrot <jean-christophe.maigrot@steeple.fr>
2024-09-01 12:39:20 +0200

    feat(nvim): Disable copilot plugin temporarily

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,13 +1,13 @@
 return {
-	"github/copilot.vim",
-	config = function()
-		vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
-			expr = true,
-			replace_keycodes = false,
-		})
-
-		vim.g.copilot_no_tab_map = false
-
-		vim.keymap.set("i", "<C-l>", "<Plug>(copilot-accept-word)")
-	end,
+	-- 	"github/copilot.vim",
+	-- 	config = function()
+	-- 		vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
+	-- 			expr = true,
+	-- 			replace_keycodes = false,
+	-- 		})
+	--
+	-- 		vim.g.copilot_no_tab_map = false
+	--
+	-- 		vim.keymap.set("i", "<C-l>", "<Plug>(copilot-accept-word)")
+	-- 	end,
 }
