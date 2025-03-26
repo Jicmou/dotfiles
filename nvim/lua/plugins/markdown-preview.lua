adf8493158a35a93c653f3d9e579de092182962a
jicmou <jc.maigrot@gmail.com>
2025-03-26 18:06:40 +0100

    fix(nvim): install markdown preview without yarn

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,7 +1,9 @@
 return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && yarn install",
+	build = function()
+		vim.fn["mkdp#util#install"]()
+	end,
 	ft = { "markdown" },
 	config = function()
 		vim.g.mkdp_filetypes = { "markdown" }
