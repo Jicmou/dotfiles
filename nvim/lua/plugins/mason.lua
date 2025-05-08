18f962f1d5c8f58e78b1ea9293e82345442ccc09
jicmou <jc.maigrot@gmail.com>
2025-05-08 23:30:29 +0200

    chore(nvim): rename mason plugin scope

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,8 +1,8 @@
 return {
 	-- package manager
-  -- Easily install and manage LSP servers, DAP servers, linters, and formatters
-	"williamboman/mason.nvim",
-  config = function ()
-    require("mason").setup()
-  end,
+	-- Easily install and manage LSP servers, DAP servers, linters, and formatters
+	"mason-org/mason.nvim",
+	config = function()
+		require("mason").setup()
+	end,
 }
