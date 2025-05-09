fa970ee93574124ec055c4229b209f3f10060aff
jicmou <jc.maigrot@gmail.com>
2025-05-10 00:35:02 +0200

    feat(nvim): add most used lsps
    Those configurations are shamelessly copied from "neovim/lspconfig"
    
    - css_ls
    - docker_compose_ls
    - docker_ls
    - html_ls
    - marksman
    - yaml_ls

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -2,4 +2,16 @@
 vim.diagnostic.config({ virtual_text = { current_line = true } })
 
 -- enable lsp
-vim.lsp.enable({ "bash_ls", "json_ls", "lua_ls", "ts_ls", "vue_ls" })
+vim.lsp.enable({
+	"bash_ls",
+	"css_ls",
+	"docker_compose_ls",
+	"docker_ls",
+	"html_ls",
+	"json_ls",
+	"lua_ls",
+	"marksman",
+	"ts_ls",
+	"vue_ls",
+	"yaml_ls",
+})
